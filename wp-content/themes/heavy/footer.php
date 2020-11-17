ca8c5ec8ff6ec26f3407f385778d22553ab05a44
Tyler J. Perry <iamtylerjordan@gmail.com>
2020-11-17 00:37:50 -0500

    styling footer

--- wp-content/themes/heavy/footer.php
+++ wp-content/themes/heavy/footer.php
@@ -11,10 +11,37 @@
 
 ?>
 
-	<footer class="site-footer">
-		<div class="site-info">
-			<a class="btn">contact us</a>
-			
+	<footer class="footer">
+		<div class="footer-container no-gutters d-md-flex">
+			<div class="footer-col d-md-none">
+				<a class="btn"><span class="btn-text">Contact Us</span><span class="btn-arrow"></span></a>
+			</div>
+			<div class="footer-col-wrap col-md d-md-flex">
+				<div class="footer-col col-md">
+					<h5>philadelphia</h5>
+					<p>200 Locust Street, Unit 7CN<p/>
+					<p>Philadelphia, PA 19106<p/><br>
+					<p><a href="tel:2159482451">(215) 948-2451</a><p/>
+				</div>
+				<div class="footer-col col-md">
+					<h5>san diego</h5>
+					<p>2869 Historic Decatur Road<p/>
+					<p>San Diego, CA 92106<p/>
+				</div>
+			</div>
+			<div class="footer-col-wrap col-md d-md-flex">
+				<div class="footer-col col-md">
+					<ul>
+						<li><a href="">Instagram</a></li>
+						<li><a href="">Twitter</a></li>
+						<li><a href="">LinkedIn</a></li>
+						<li><a href="">Facebook</a></li>
+					</ul>
+				</div>
+				<div class="footer-col d-none d-md-block">
+					<a class="btn"><span class="btn-text">Contact Us</span><span class="btn-arrow"></span></a>
+				</div>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- footer -->
 </div><!-- #page -->
